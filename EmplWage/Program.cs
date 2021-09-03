ab65e5a8ba916474e9493c7e558ed3c7c81ab9b2
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 15:23:17 +0530

    UC-2 Full Time Emp Daily Wage

--- EmplWage/Program.cs
+++ EmplWage/Program.cs
@@ -9,10 +9,16 @@ namespace EmplWage
             Console.WriteLine("Wel-Come to Employee Wage Computation Program");
             Random random = new Random();
             int empCheck = random.Next(0, 2);
+
+            int Full_Time = 8;
+            int Per_Hour_Sal = 20;
+            int One_Day_Sal=0;
             
             if (empCheck==0)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Full Time");
+                One_Day_Sal = Per_Hour_Sal * Full_Time;
+                Console.WriteLine("One Day Salary is: {0}", One_Day_Sal);
             }
             else
             {
