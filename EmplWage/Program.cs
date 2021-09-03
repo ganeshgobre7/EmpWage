1991201ec349121b23e2286425d0cfa18dcb3da1
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 16:07:31 +0530

    UC-5 Calculate Month Wage

--- EmplWage/Program.cs
+++ EmplWage/Program.cs
@@ -14,6 +14,8 @@ namespace EmplWage
             int Part_Time = 4;
             int Per_Hour_Sal = 20;
             int One_Day_Sal=0;
+            int Total_Sal = 0;
+            int No_Work_Day = 20;
             
             switch(empCheck)
             {
@@ -22,6 +24,8 @@ namespace EmplWage
                     Console.WriteLine("Employee is Full Time");
                     One_Day_Sal = Per_Hour_Sal * Full_Time;
                     Console.WriteLine("One Day Salary of Full Time Employee: {0}", One_Day_Sal);
+                    Total_Sal = One_Day_Sal * No_Work_Day;
+                    Console.WriteLine("Total Salary Of Full Time Employee in a Month: {0}", Total_Sal);
                     break;
 
                 case 2:
@@ -29,6 +33,8 @@ namespace EmplWage
                     Console.WriteLine("Employee is Part Time");
                     One_Day_Sal = Per_Hour_Sal * Part_Time;
                     Console.WriteLine("One Day Salary of Part Time Employee: {0}", One_Day_Sal);
+                    Total_Sal = One_Day_Sal * No_Work_Day;
+                    Console.WriteLine("Total Salary Of Part Time Employee in a Month: {0}", Total_Sal);
                     break;
 
                 default:
