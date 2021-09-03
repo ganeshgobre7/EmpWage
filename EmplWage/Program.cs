53734eecbfa5ad1e15de98ffde219ed6c8be75a0
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 15:53:26 +0530

    UC-4 Using Switch Case

--- EmplWage/Program.cs
+++ EmplWage/Program.cs
@@ -15,18 +15,30 @@ namespace EmplWage
             int Per_Hour_Sal = 20;
             int One_Day_Sal=0;
             
-            if (empCheck==0)
+            switch(empCheck)
             {
-                Console.WriteLine("Employee is Full Time");
-                One_Day_Sal = Per_Hour_Sal * Full_Time;
-                Console.WriteLine("One Day Salary of Full Time Employee: {0}", One_Day_Sal);
-            }
-            else
-            {
-                Console.WriteLine("Employee is Part Time");
-                One_Day_Sal = Per_Hour_Sal * Part_Time;
-                Console.WriteLine("One Day Salary of Part Time Employee: {0}", One_Day_Sal);
-            }
+                case 1:
+
+                    Console.WriteLine("Employee is Full Time");
+                    One_Day_Sal = Per_Hour_Sal * Full_Time;
+                    Console.WriteLine("One Day Salary of Full Time Employee: {0}", One_Day_Sal);
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Employee is Part Time");
+                    One_Day_Sal = Per_Hour_Sal * Part_Time;
+                    Console.WriteLine("One Day Salary of Part Time Employee: {0}", One_Day_Sal);
+                    break;
+
+                default:
+                    Console.WriteLine("Exit");
+                    break;
+
+            }  
+               
+                
+            
         }
     }
 }
