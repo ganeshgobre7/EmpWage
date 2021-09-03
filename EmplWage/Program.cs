2a5cd60c1c7ca4e78fb463ed632ef9c79c8e5951
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 15:40:23 +0530

    UC-3 Part Time Emp Daily Wage

--- EmplWage/Program.cs
+++ EmplWage/Program.cs
@@ -11,6 +11,7 @@ namespace EmplWage
             int empCheck = random.Next(0, 2);
 
             int Full_Time = 8;
+            int Part_Time = 4;
             int Per_Hour_Sal = 20;
             int One_Day_Sal=0;
             
@@ -18,11 +19,13 @@ namespace EmplWage
             {
                 Console.WriteLine("Employee is Full Time");
                 One_Day_Sal = Per_Hour_Sal * Full_Time;
-                Console.WriteLine("One Day Salary is: {0}", One_Day_Sal);
+                Console.WriteLine("One Day Salary of Full Time Employee: {0}", One_Day_Sal);
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee is Part Time");
+                One_Day_Sal = Per_Hour_Sal * Part_Time;
+                Console.WriteLine("One Day Salary of Part Time Employee: {0}", One_Day_Sal);
             }
         }
     }
