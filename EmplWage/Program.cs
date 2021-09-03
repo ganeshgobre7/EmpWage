5f9321c7578d5ee670cf0ae77accb11857ed0226
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-03 14:53:20 +0530

    UC-1 Employee Present or Absent

--- EmplWage/Program.cs
+++ EmplWage/Program.cs
@@ -7,6 +7,17 @@ namespace EmplWage
         static void Main(string[] args)
         {
             Console.WriteLine("Wel-Come to Employee Wage Computation Program");
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            
+            if (empCheck==0)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
